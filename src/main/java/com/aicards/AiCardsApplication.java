1e0b2c5b01956dbe059d9ef8d9a6554626e21b91
David <davidpenalives@gmail.com>
2023-05-20 19:33:33 -0300

    init

---
+++ src/main/java/com/aicards/AiCardsApplication.java
@@ -0,0 +1,13 @@
+package com.aicards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aicards/AiCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
