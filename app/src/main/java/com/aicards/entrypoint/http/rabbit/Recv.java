cdd4e741652dfc6161ec3ab57e3f91b8807e7f24
enlaitc <tccdevop@gmail.com>
2023-05-25 16:27:45 -0300

    teste rabbitmq

---
+++ app/src/main/java/com/aicards/entrypoint/http/rabbit/Recv.java
@@ -0,0 +1,28 @@
+package com.aicards.entrypoint.http.rabbit;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Recv {
+
+    private static final String QUEUE_NAME = "teste";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        System.out.println("Waiting for messages.");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("Received '" + message + "'");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
